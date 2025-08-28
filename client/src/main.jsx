e474343508c539fef1cc0f55875f4b36d01e7407
jaadu611 <ayushburman281@gmail.com>
2025-08-28 15:20:23 +0530

    fixed viewport

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>
-);
+function Root() {
+  useEffect(() => {
+    const appWrapper = document.getElementById("root");
+
+    function updateHeight() {
+      if (appWrapper) appWrapper.style.height = `${window.innerHeight}px`;
+    }
+
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Root />);
